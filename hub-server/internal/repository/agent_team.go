044c3a374389064bb6f994385b6a995be97b3755
Delicious233 <101502465+DeliciousBuding@users.noreply.github.com>
2026-05-27 05:11:43 +0800

    feat(hub): index team run artifacts

--- hub-server/internal/repository/agent_team.go
+++ hub-server/internal/repository/agent_team.go
@@ -189,6 +189,26 @@ func UpdateTeamTaskDispatchBinding(db *gorm.DB, id, pendingTaskID string) error 
 	}).Error
 }
 
+// AgentTeamArtifact
+
+func ReplaceTeamArtifactsForRun(db *gorm.DB, teamRunID string, artifacts []model.AgentTeamArtifact) error {
+	return db.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Where("team_run_id = ?", teamRunID).Delete(&model.AgentTeamArtifact{}).Error; err != nil {
+			return err
+		}
+		if len(artifacts) == 0 {
+			return nil
+		}
+		return tx.Create(&artifacts).Error
+	})
+}
+
+func ListTeamArtifactsByRun(db *gorm.DB, teamRunID string) ([]model.AgentTeamArtifact, error) {
+	var artifacts []model.AgentTeamArtifact
+	err := db.Where("team_run_id = ?", teamRunID).Order("created_at ASC, id ASC").Find(&artifacts).Error
+	return artifacts, err
+}
+
 // AgentTeamEvent
 
 func AppendTeamEvent(db *gorm.DB, event *model.AgentTeamEvent) error {
